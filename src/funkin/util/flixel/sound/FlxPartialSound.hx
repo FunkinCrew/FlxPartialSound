1ad3dccfb80ee957ef904f5403060e5206846751
Hyper_ <40342021+NotHyper-474@users.noreply.github.com>
2025-09-30 21:25:37 -0300

    Fix deprecation warning

--- src/funkin/util/flixel/sound/FlxPartialSound.hx
+++ src/funkin/util/flixel/sound/FlxPartialSound.hx
@@ -362,7 +362,7 @@ class FlxPartialSound
 		var threadPool = new ThreadPool();
 		var bytes:Null<Bytes> = null;
 
-		function doWork(state:Dynamic)
+		function doWork(state:Dynamic, workOutput:Dynamic)
 		{
 			if (!Assets.exists(path) || path == null)
 				threadPool.sendError({path: path, promise: promise, error: "ERROR: Failed to load bytes for Asset " + path + " Because it dosen't exist."});
@@ -402,12 +402,11 @@ class FlxPartialSound
 			promise.complete(bytes);
 		}
 
-		threadPool.doWork.add(doWork);
 		threadPool.onProgress.add(onProgress);
 		threadPool.onComplete.add(onComplete);
 		threadPool.onError.add((state:Dynamic) -> promise.error({error: state.error, responseData: null}));
 
-		threadPool.queue({});
+		threadPool.queue(doWork);
 
 		return promise.future;
 	}
